f0e263717abb0fb302595fb7729856b1e362a59d
KorobskoyRoman <shopone023@gmail.com>
2022-08-13 17:59:47 +0300

    Initial Commit

---
+++ Profiles-MVP-CoreData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Profiles-MVP-CoreData
+//
+//  Created by Roman Korobskoy on 13.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
